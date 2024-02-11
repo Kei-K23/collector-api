cc89c9e9d229e792f6edfa63a65ad22c8c0aef8e
Kei-K23 <keiksl2301@gmail.com>
2024-02-11 21:43:16 +0630

    add update and delete service for user

--- services/user.service.ts
+++ services/user.service.ts
@@ -1,5 +1,9 @@
 import { db } from "../lib/db";
-import { CreateUserSchema } from "../schema/user.schema";
+import {
+  CreateUserSchema,
+  DeleteUserSchema,
+  UpdateUserSchema,
+} from "../schema/user.schema";
 
 export async function createUser({ data }: { data: CreateUserSchema }) {
   try {
@@ -8,5 +12,32 @@ export async function createUser({ data }: { data: CreateUserSchema }) {
     });
   } catch (e: any) {
     throw new Error("Something went wrong when creating user.");
+  }
+}
+
+export async function updateUser({ data }: { data: UpdateUserSchema }) {
+  try {
+    return await db.user.update({
+      where: {
+        id: data.params.userId,
+      },
+      data: {
+        ...data.body,
+      },
+    });
+  } catch (e: any) {
+    throw new Error("Something went wrong when updating user.");
+  }
+}
+
+export async function deleteUser({ userId }: DeleteUserSchema) {
+  try {
+    return await db.user.delete({
+      where: {
+        id: userId,
+      },
+    });
+  } catch (e: any) {
+    throw new Error("Something went wrong when deleting user.");
   }
 }
